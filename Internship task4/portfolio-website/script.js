1444617f04435a8c27a37a2f3f3e0287f0e93ee8
rahul medikonda <rahulmedikonda48534@gmail.com>
2026-06-22 22:47:44 +0530

    Delete Internship task4 directory

--- Internship task4/portfolio-website/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit",function(e){
-    e.preventDefault();
-
-    document.getElementById("successMessage").innerText =
-    "Message sent successfully!";
-});
